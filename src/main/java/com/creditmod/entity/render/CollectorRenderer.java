af9cb0cc6cac6385b82d465e616c3978f2787c6c
123ewqefr <128382120+123ewqefr@users.noreply.github.com>
2026-07-07 13:12:18 +0300

    Update CollectorRenderer.java

--- src/main/java/com/creditmod/entity/render/CollectorRenderer.java
+++ src/main/java/com/creditmod/entity/render/CollectorRenderer.java
@@ -4,16 +4,16 @@ import com.creditmod.entity.CollectorEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.renderer.entity.model.ZombieModel;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.util.ResourceLocation;
 
-public class CollectorRenderer extends MobRenderer<CollectorEntity, ZombieModel<CollectorEntity>> {
+public class CollectorRenderer extends MobRenderer<CollectorEntity, BipedModel<CollectorEntity>> {
 
     private static final ResourceLocation TEXTURE =
             new ResourceLocation("minecraft", "textures/entity/zombie/zombie.png");
 
     public CollectorRenderer(EntityRendererManager manager) {
-        super(manager, new ZombieModel<>(0, false), 0.5F);
+        super(manager, new BipedModel<>(0.0F), 0.5F);
     }
 
     @Override
@@ -23,7 +23,6 @@ public class CollectorRenderer extends MobRenderer<CollectorEntity, ZombieModel<
 
     @Override
     protected void scale(CollectorEntity entity, MatrixStack stack, float ticks) {
-        // Коллекторы немного крупнее обычных зомби
         stack.scale(1.1F, 1.1F, 1.1F);
     }
 }
